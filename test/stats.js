46f97c8b46012d4c0bb348d8caa9429cc45c2552
Jake Luer <jake.luer@gmail.com>
2012-05-04 15:02:23 -0400

    moved almost equal / deep almost equal in from core

--- test/stats.js
+++ test/stats.js
@@ -4,7 +4,8 @@ if (!chai) {
   chai.use(stats);
 }
 
-var should = chai.should();
+var should = chai.should()
+  , assert = chai.assert;
 
 describe('Chai Stats', function () {
 
@@ -22,6 +23,36 @@ describe('Chai Stats', function () {
     it('should be able to get the standard deviation', function () {
       nums.should.have.stdev.equal(3.1622776601683795);
       nums.should.have.deviation.equal(3.1622776601683795);
+    });
+  });
+
+  describe('almost equal / eql', function () {
+    it('should work: .almost.equal // almostEqual', function() {
+      (3.1416).should.almost.equal(3.14159, 3);
+      assert.almostEqual(3.1416, 3.14159, 3);
+
+      (function () {
+        assert.almostEqual(3.1416, 3.14159, 6);
+      }).should.throw(chai.AssertionError, "expected 3.1416 to equal 3.14159 up to 6 decimal places");
+
+      (function () {
+        assert.almostEqual(3.1416, 3.14159);
+      }).should.throw(chai.AssertionError,  "expected 3.1416 to equal 3.14159 up to 7 decimal places");
+
+    });
+
+    it('should work: .almost.eql // deepAlmostEqual', function() {
+      ({ pi: 3.1416 }).should.almost.eql({ pi: 3.14159 }, 3);
+      assert.deepAlmostEqual({pi: 3.1416}, {pi: 3.14159}, 3);
+
+      (function () {
+        assert.deepAlmostEqual({pi: 3.1416}, {pi: 3.14159}, 6);
+      }).should.throw(chai.AssertionError, "expected { pi: 3.1416 } to equal { pi: 3.14159 } up to 6 decimal places");
+
+      (function () {
+        assert.deepAlmostEqual({pi: 3.1416}, {pi: 3.14159});
+      }).should.throw(chai.AssertionError, "expected { pi: 3.1416 } to equal { pi: 3.14159 } up to 7 decimal places");
+
     });
   });
 });
