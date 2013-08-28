0a9a8af9c443eab6f691e424c934303f33ba5827
James Talmage <james.talmage@jrtechnical.com>
2013-08-28 00:55:52 -0400

    Make math and 'almost' functions publicly available.
    Correct coding style errors in commit 8abe3ec
    
    fix indentation
    
    Add rebuilt file

--- test/stats.js
+++ test/stats.js
@@ -1,17 +1,23 @@
+var tmp;
 if (!chai) {
   var chai = require('chai')
     , stats = require('..');
   chai.use(stats);
+  tmp = stats;
+}
+else {
+  tmp = chai_stats;
 }
 
 var should = chai.should()
   , assert = chai.assert
-  , expect = chai.expect;
+  , expect = chai.expect
+  , chai_stats = tmp;
 
 describe('Chai Stats', function () {
 
   describe('Math Basics', function () {
-    var nums = [ 1, 3, 5, 7, 9, ];
+    var nums = [ 1, 3, 5, 7, 9 ];
 
     it('should be able to get the sum of a set of numbers', function () {
       nums.should.have.sum.equal(25);
@@ -64,5 +70,30 @@ describe('Chai Stats', function () {
       expect(3.1415).to.not.almost.equal(3); // precision undefined => 7 decimal places
       expect(5.8).to.almost.equal(6, 0); // precision 0 => rounding
     });
+  });
+
+  describe('math functions are publicly available',function(){
+
+    var nums = [ 1, 3, 5, 7, 9 ];
+
+    it('almostEqual',function(){
+      expect(chai_stats.almostEqual(3.1415,3,0)).to.be.true;
+      expect(chai_stats.almostEqual(3.1415,3.1416,3)).to.be.true;
+      expect(chai_stats.almostEqual(3.1415,3.1416,4)).to.be.false;
+    });
+    it('deepAlmostEqual',function(){
+      expect(chai_stats.deepAlmostEqual({ pi: 3.1416 },{ pi: 3 },0)).to.be.true;
+      expect(chai_stats.deepAlmostEqual({ pi: 3.1416 },{ pi: 3.14159 },4)).to.be.true;
+      expect(chai_stats.deepAlmostEqual({ pi: 3.1416 },{ pi: 3.14159 },5)).to.be.false;
+    });
+    it('mean',function(){
+      expect(chai_stats.mean(nums)).to.equal(5);
+    });
+    it('sum',function(){
+      expect(chai_stats.sum(nums)).to.equal(25);
+    });
+    it('sdeviation',function(){
+      expect(chai_stats.sdeviation(nums)).to.equal(3.1622776601683795);
+    })
   });
 });
